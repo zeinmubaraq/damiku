0c78241c722a03f48ab69a807c45f7c8f7bebcfa
zeinmubaraq <zainmubaraq@gmail.com>
2022-04-18 22:45:59 +0700

    initial commit

---
+++ utils/post.ts
@@ -0,0 +1,16 @@
+export const post = [
+  {
+    id: 1,
+    slug: 'lorem-ipsum-dolor-sit-amet',
+    title: 'Lorem Ipsum Dolor Sit Amet',
+    category: 'technology',
+    body: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut justo nisl, eleifend eu ex quis, pharetra pharetra ante. Proin sodales ligula sit amet lectus sodales, a sodales justo tincidunt. Etiam dapibus mollis turpis at luctus. Phasellus commodo pretium egestas. Vivamus euismod id lectus eget malesuada. Phasellus lacinia arcu sed ex ornare porttitor. Vivamus porta dui a massa auctor facilisis. Vivamus at risus urna.',
+  },
+  {
+    id: 2,
+    slug: 'Nullam-Tellus-Ex-Malesuada-ac',
+    title: 'Nullam Tellus Ex Malesuada ac',
+    category: 'technology',
+    body: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed eget auctor mauris. Nullam odio eros, molestie in varius a, tempus quis risus. Nunc velit tortor, consectetur a vulputate et, pulvinar et elit. Sed nec ornare mauris, quis pharetra ante. Ut turpis augue, commodo vitae dolor at, ornare viverra diam. Suspendisse bibendum magna erat. Proin tempus, ante in tincidunt blandit, quam erat dictum justo, cursus sodales ex augue non est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras vel rutrum risus, vel aliquam neque. Nam a massa elementum, ultricies tellus vel, congue enim.',
+  },
+];
---
+++ utils/user.ts
@@ -0,0 +1,8 @@
+export const user = [
+  {
+    id: 1,
+    name: 'Valentino Jurken',
+    username: 'vjurken',
+    email: 'vjurken@test.dev',
+  },
+];
