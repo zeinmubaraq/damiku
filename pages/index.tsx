31fb54ed936d461f6e7a605c810ce3368c42a813
zeinmubaraq <zainmubaraq@gmail.com>
2022-09-22 17:59:19 +0700

    adding navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
-    <div className="w-screenpx-10 h-screen pt-48 lg:px-20">
+    <div className="h-screen w-screen px-10 pt-48 lg:px-20">
       <Head>
         <title>Damiku</title>
         <meta name="description" content="Dummy API gratis untuk testing dan prototyping" />
@@ -15,6 +16,14 @@ const Home: NextPage = () => {
         <p className="text-2xl font-semibold lg:text-3xl">
           Dummy API gratis untuk testing dan prototyping.
         </p>
+        <div className="flex space-x-3 text-xl">
+          <Link href="/api/post">
+            <a className="text-blue-700 hover:underline hover:underline-offset-4">Post</a>
+          </Link>
+          <Link href="/api/user">
+            <a className="text-blue-700 hover:underline hover:underline-offset-4">User</a>
+          </Link>
+        </div>
       </div>
     </div>
   );
